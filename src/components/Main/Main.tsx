0394eea27f847abaf40434fffc729e44eb842349
Nam Vuong <thain868@gmail.com>
2025-01-21 13:58:59 +0700

    #1 - Add encode section

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import styles from "./Main.module.css"
 
 import { Header } from "../Header/Header";
+import { Base64Encode } from "../Base64/Base64Encode";
 
 interface MainProps {
 
@@ -11,5 +12,6 @@ export const Main: FC<MainProps> = (props:MainProps) => {
 
     return <div className={styles.main}>
         <Header/>
+        <Base64Encode/>
     </div>
 }
