bf6a6d22065b8b86c0753b6e5979c124ea26fbd2
Nam Vuong <thain868@gmail.com>
2025-01-22 07:53:23 +0700

    #1 - Fix using the same ids in EncodeOptions tag

--- src/components/EncodeOptions/EncodeOptions.tsx
+++ src/components/EncodeOptions/EncodeOptions.tsx
@@ -8,35 +8,36 @@ interface EncodeOptionsProps {
 
 export const EncodeOptions: FC<EncodeOptionsProps> = (props:EncodeOptionsProps) => {
     const[t] = useTranslation("global");
+    const random:number = Math.random();
 
     return <div className={styles.encodeOptions}>
         <div>
-            <select name="destinationCharacterSet" id="destinationCharacterSet">
+            <select name="destinationCharacterSet" id={"destinationCharacterSet"+random}>
                 <option value="volvo">Volvo</option>
                 <option value="saab">Saab</option>
                 <option value="mercedes">Mercedes</option>
                 <option value="audi">Audi</option>
             </select>
-            <label htmlFor="destinationCharacterSet">{t("base64Encode.options.destinationCharacterSet")}</label>
+            <label htmlFor={"destinationCharacterSet"+random}>{t("base64Encode.options.destinationCharacterSet")}</label>
         </div>
         <div>
-            <select name="destinationNewline" id="destinationNewline">
+            <select name="destinationNewline" id={"destinationNewline"+random}>
                 <option value="volvo">LF (Unix)</option>
                 <option value="audi">CRLF (Windows)</option>
             </select>
-            <label htmlFor="destinationNewline">{t("base64Encode.options.destinationNewline")}</label>
+            <label htmlFor={"destinationNewline"+random}>{t("base64Encode.options.destinationNewline")}</label>
         </div>
         <div>
-            <input type="checkbox" name="" id="encodeSeparately" />
-            <label htmlFor="encodeSeparately">{t("base64Encode.options.encodeSeparately")}</label>
+            <input type="checkbox" name="" id={"encodeSeparately"+random} />
+            <label htmlFor={"encodeSeparately"+random}>{t("base64Encode.options.encodeSeparately")}</label>
         </div>
         <div>
-            <input type="checkbox" name="" id="splitLines" />
-            <label htmlFor="splitLines">{t("base64Encode.options.splitLines")}</label>
+            <input type="checkbox" name="" id={"splitLines"+random} />
+            <label htmlFor={"splitLines"+random}>{t("base64Encode.options.splitLines")}</label>
         </div>
         <div>
-            <input type="checkbox" name="" id="urlSafe" />
-            <label htmlFor="urlSafe">{t("base64Encode.options.urlSafe")}</label>
+            <input type="checkbox" name="" id={"urlSafe"+random} />
+            <label htmlFor={"urlSafe"+random}>{t("base64Encode.options.urlSafe")}</label>
         </div>
     </div>
 }
